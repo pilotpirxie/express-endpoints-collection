0b10054f2eae4304fa97ae6dc74c81dc6c494b2a
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2024-08-16 16:04:59 +0200

    Do not validate output

--- src/index.ts
+++ src/index.ts
@@ -38,35 +38,6 @@ export class EndpointsCollection {
     };
   }
 
-  private static validateOutput(schema: EndpointOutputSchema) {
-    return (req: Request, res: Response, next: NextFunction) => {
-      const originalJson = res.json;
-
-      // @ts-ignore
-      res.json = (body: any) => {
-        const status = res.statusCode;
-        const schemaForStatus = schema.find((s) => s.status === status);
-
-        try {
-          if (schemaForStatus?.body) {
-            schemaForStatus.body.parse(body);
-          }
-          originalJson.call(res, body);
-        } catch (error) {
-          if (error instanceof z.ZodError) {
-            console.error("Error in response validation", error.errors);
-            return res.status(500).json({
-              error: "Internal server error",
-            });
-          }
-          next(error);
-        }
-      };
-
-      next();
-    };
-  }
-
   public callOriginal(
     method: HttpMethod,
     path: string,
@@ -103,10 +74,6 @@ export class EndpointsCollection {
     }
 
     combinedHandlers.push(handlers);
-
-    if (outputSchema) {
-      combinedHandlers.push(EndpointsCollection.validateOutput(outputSchema));
-    }
 
     if (beforeResponse) {
       combinedHandlers.push(beforeResponse);
